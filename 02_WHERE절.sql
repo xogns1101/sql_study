4914537e113e606cd94e374473c024862312ccf1
taehun <>
2024-03-08 17:31:21 +0900

    sql수업시작

---
+++ 02_WHERE절.sql
@@ -0,0 +1,11 @@
+
+SELECT * FROM employees;
+
+-- WHERE 절 비교 (데이터 값은 대/소문자를 구분합니다.)
+SELECT first_name, last_name, job_id
+FROM employees
+WHERE job_id = 'IT_PROG';
+
+SELECT *
+FROM employees
+WHERE last_name = 'King';
